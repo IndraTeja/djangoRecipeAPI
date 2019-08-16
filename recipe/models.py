4a8ec3ca8820f68a7df5307ecd999187db04c9d6
Indra Chintakayala <ichintakayala@unomaha.edu>
2019-08-15 19:56:57 -0500

    Recipe model created, admin registed

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=50, null=False, unique=True, blank=False, primary_key=True)
+    author = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.author)
