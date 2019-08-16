9952352caaccdb32f10e0ffb613b1c438b7a7b7e
Indra Chintakayala <ichintakayala@unomaha.edu>
2019-08-15 20:35:48 -0500

    added viewsets

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .serializers import *
+from .models import *
+from rest_framework import viewsets
 
-# Create your views here.
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class StepViewSet(viewsets.ModelViewSet):
+    queryset = Step.objects.all()
+    serializer_class = StepSerializer
+    
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
